e10717a63852f36be67e7b029179ad8cb3806b9d
Manav Gagvani <mgagvani@outlook.com>
2022-08-30 10:16:08 -0400

    eoc (end of class) commit (problems 1-5 working)

--- Unit0/Data Structures Challenge/datastructures.py
+++ Unit0/Data Structures Challenge/datastructures.py
@@ -20,9 +20,44 @@ def main():
 
     ## Problems
     # Problem 1
-    print("1: " ,len(set1.union(set2)))
+    print("1: " ,len(set1.intersection(set2)))
 
     # Problem 2
+    count = 0
+    set1_ordered = []
+    for i, val in enumerate(list1):
+        if not val in set1_ordered:
+            set1_ordered.append(val)
+    for i, val in enumerate(set1_ordered):
+        if i%100 == 99:
+            count += val
+    print("2: ", count)
+
+    # Problem 3
+    t = 0
+    for value in set3:
+        if value in dict1.keys():
+            t += dict1[value]
+        if value in dict2.keys():
+            t += dict2[value]
+    print("3: ",t)
+
+    # Problem 4
+    print("4: ",sorted(list(set1))[:10])
+
+    # Problem 5
+    print("5: ",list(reversed(sorted([num for num in dict2.keys() if dict2[num] > 1])[-10:])))
+
+    # Problem 6
+    curridx, previdx, seq = 0, 0, set()
+    for i, val in enumerate(list1):
+        if val % 53 == 0:
+            previdx = curridx
+            curridx = i
+            seq.add(min(list1[previdx:curridx+1]))
+    print("6: ", sum(seq))
+
+        
     
 
 
