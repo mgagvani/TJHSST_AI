b7efbe484afae6c664d3d36b67af5d814743e5a1
Manav Gagvani <mgagvani@outlook.com>
2022-09-19 09:37:00 -0400

    working bfs algorithm

--- Unit1a/Sliding Puzzles/bfs.py
+++ Unit1a/Sliding Puzzles/bfs.py
@@ -42,15 +42,20 @@ def find_goal(string):
 
 
 def swap(sourcei, sourcej, desti, destj, mat, size):
-    newmat = to_mat(to_str(mat), size)
+    # newmat = to_mat(to_str(mat), size)
     # print(sourcei, sourcej, desti, destj)
-    source = newmat[sourcei][sourcej]
-    dest = newmat[desti][destj]
+    source = mat[sourcei][sourcej]
+    dest = mat[desti][destj]
     # print(source, dest)
     # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    newmat[sourcei][sourcej] = dest
-    newmat[desti][destj] = source
-    return to_str(newmat)
+    mat[sourcei][sourcej] = dest
+    mat[desti][destj] = source
+    toRet =  to_str(mat)
+    # swap back
+    mat[sourcei][sourcej] = source
+    mat[desti][destj] = dest
+    return toRet
+    
 
 
 def get_children(str, size):
@@ -101,17 +106,27 @@ def bfs(start, goal, size): # pass in strings!! (and the size of the string)
     # print("_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
     fringe = deque()
     visited = set()
-    fringe.append(start)
+    fringe.append((start, 0))
     visited.add(start)
     while len(fringe) > 0:
-        v = fringe.popleft()
+        v, _l = fringe.popleft() # DIS IS PARENT
         if v == goal:
-            return v, i
+            return v, _l
         for child in get_children(v, size):
-            # print(matstr(to_mat(child, size)))
+            # print(matstr(to_mat(child, size)))  
             if child not in visited:
-                fringe.append(child)
+                fringe.append((child, _l + 1)) # ADD ONE TO PARENT"S LEVEL
                 visited.add(child)
+
+
+def backtrack(c, parents, start):
+    i = 0
+    child = c
+    while parents[child] != start:
+        child = parents[child]
+        i += 1
+    return i
+
 
 if __name__ == "__main__":
     args = sys.argv
@@ -126,9 +141,13 @@ if __name__ == "__main__":
         size, start = load_string(line)
         goal = find_goal(start)
         # print(f"goal: {goal}")
-        _found, num = bfs(start, goal, size)
-
         b = time.perf_counter()
-        print(f"Line {i}: {to_str(start)},  {_found}  found in {num} moves and {b-a} seconds")
+
+        _found, level = bfs(start, goal, size)
+        # steps = backtrack(goal, parents, start)
+
+
+        c = time.perf_counter()
+        print(f"Line {i}: {to_str(start)}, {level} moves found in {c-b} seconds")
         
 
