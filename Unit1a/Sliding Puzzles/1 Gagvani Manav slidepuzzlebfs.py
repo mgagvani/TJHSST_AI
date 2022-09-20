6b07e92ca3168f56419eb31294cc243222192d4a
Manav Gagvani <mgagvani@outlook.com>
2022-09-20 13:53:40 +0000

    get proper path lengths for bfs brainteaser questions

--- Unit1a/Sliding Puzzles/1 Gagvani Manav slidepuzzlebfs.py
+++ Unit1a/Sliding Puzzles/1 Gagvani Manav slidepuzzlebfs.py
@@ -1,8 +1,7 @@
 import sys
 import time
 from collections import deque
-# from itertools import permutations
-
+from itertools import permutations
 
 
 def load_string(line):
@@ -21,7 +20,7 @@ def to_mat(str, size):
 
 
 def to_str(mat):
-    s = ''
+    s = ""
     for row in mat:
         for val in row:
             s += val
@@ -29,18 +28,18 @@ def to_str(mat):
 
 
 def matstr(mat):
-    s = ''
+    s = ""
     for row in mat:
         for val in row:
-            s += val + ' '
-        s += '\n'
+            s += val + " "
+        s += "\n"
     return s
 
 
 def find_goal(string):
     a = sorted(string)[1:]
-    a.append('.')
-    return ''.join(a)
+    a.append(".")
+    return "".join(a)
 
 
 def swap(sourcei, sourcej, desti, destj, mat, size):
@@ -52,12 +51,11 @@ def swap(sourcei, sourcej, desti, destj, mat, size):
     # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     mat[sourcei][sourcej] = dest
     mat[desti][destj] = source
-    toRet =  to_str(mat)
+    toRet = to_str(mat)
     # swap back
     mat[sourcei][sourcej] = source
     mat[desti][destj] = dest
     return toRet
-    
 
 
 def get_children(str, size):
@@ -84,34 +82,46 @@ def get_children(str, size):
         a = str
         ch1 = str[idx]
         ch2 = str[idx + 1]
-        a = a.replace(ch2, '!',) # just a temporary
+        a = a.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         a = a.replace(ch1, ch2)
-        a = a.replace('!', ch1)
+        a = a.replace("!", ch1)
         children.append(a)
-    elif (idx+1) % size == 0:
+    elif (idx + 1) % size == 0:
         # swap with left neighbour
         b = str
         ch1 = str[idx]
         ch2 = str[idx - 1]
-        b = b.replace(ch2, '!',) # just a temporary
+        b = b.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         b = b.replace(ch1, ch2)
-        b = b.replace('!', ch1)
+        b = b.replace("!", ch1)
         children.append(b)
     else:
         # swap with both neigubours
         c = str
         ch1 = str[idx]
         ch2 = str[idx + 1]
-        c = c.replace(ch2, '!',) # just a temporary
+        c = c.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         c = c.replace(ch1, ch2)
-        c = c.replace('!', ch1)
+        c = c.replace("!", ch1)
         children.append(c)
         d = str
         ch1 = str[idx]
         ch2 = str[idx - 1]
-        d = d.replace(ch2, '!',) # just a temporary
+        d = d.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         d = d.replace(ch1, ch2)
-        d = d.replace('!', ch1)
+        d = d.replace("!", ch1)
         children.append(d)
 
     # vertical swapping
@@ -121,53 +131,66 @@ def get_children(str, size):
         e = str
         ch1 = str[idx]
         ch2 = str[idx + size]
-        e = e.replace(ch2, '!',) # just a temporary
+        e = e.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         e = e.replace(ch1, ch2)
-        e = e.replace('!', ch1)
+        e = e.replace("!", ch1)
         children.append(e)
     elif idx >= size * (size - 1):
         # swap with above
         f = str
         ch1 = str[idx]
         ch2 = str[idx - size]
-        f = f.replace(ch2, '!',) # just a temporary
+        f = f.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         f = f.replace(ch1, ch2)
-        f = f.replace('!', ch1)
+        f = f.replace("!", ch1)
         children.append(f)
     else:
-          # swap with both neigubours
+        # swap with both neigubours
         g = str
         ch1 = str[idx]
         ch2 = str[idx + size]
-        g = g.replace(ch2, '!',) # just a temporary
+        g = g.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         g = g.replace(ch1, ch2)
-        g = g.replace('!', ch1)
+        g = g.replace("!", ch1)
         children.append(g)
         h = str
         ch1 = str[idx]
         ch2 = str[idx - size]
-        h = h.replace(ch2, '!',) # just a temporary
+        h = h.replace(
+            ch2,
+            "!",
+        )  # just a temporary
         h = h.replace(ch1, ch2)
-        h = h.replace('!', ch1)
+        h = h.replace("!", ch1)
         children.append(h)
     return children
 
 
-def bfs(start, goal, size): # pass in strings!! (and the size of the string)
+def bfs(start, goal, size):  # pass in strings!! (and the size of the string)
     fringe = deque()
     visited = set()
     fringe.append((start, 0))
     visited.add(start)
     while len(fringe) > 0:
-        v, _l = fringe.popleft() # DIS IS PARENT
+        v, _l = fringe.popleft()  # DIS IS PARENT
         if v == goal:
             return v, _l
         for child in get_children(v, size):
-            # print(matstr(to_mat(child, size)))  
+            # print(matstr(to_mat(child, size)))
             if child not in visited:
-                fringe.append((child, _l + 1)) # ADD ONE TO PARENT"S LEVEL
+                fringe.append((child, _l + 1))  # ADD ONE TO PARENT"S LEVEL
                 visited.add(child)
     return -1
+
 
 def modBFS(size):
     if size == 2:
@@ -179,15 +202,16 @@ def modBFS(size):
     fringe.append((s, 0))
     visited.add(s)
     while len(fringe) > 0:
-        v, _l = fringe.popleft() # DIS IS PARENT
+        v, _l = fringe.popleft()  # DIS IS PARENT
         for child in get_children(v, size):
-            # print(matstr(to_mat(child, size)))  
+            # print(matstr(to_mat(child, size)))
             if child not in visited:
-                fringe.append((child, _l + 1)) # ADD ONE TO PARENT"S LEVEL
+                fringe.append((child, _l + 1))  # ADD ONE TO PARENT"S LEVEL
                 visited.add(child)
     return len(visited)
 
-def modBFS2(start): 
+
+def modBFS2(start):
     fringe = deque()
     visited = set()
     fringe.append((start, 0))
@@ -195,28 +219,46 @@ def modBFS2(start):
     counter = 0
     allv = []
     while len(fringe) > 0:
-        v, _l = fringe.popleft() # DIS IS PARENT
+        v, _l = fringe.popleft()  # DIS IS PARENT
         if _l == 31:
             counter += 1
             allv.append(v)
             continue
         for child in get_children(v, 3):
             if child not in visited:
-                fringe.append((child, _l + 1)) # ADD ONE TO PARENT"S LEVEL
+                fringe.append((child, _l + 1))  # ADD ONE TO PARENT"S LEVEL
                 visited.add(child)
     return counter, allv
 
-    
+
+def modBFS3(start, goal, size):  # pass in strings!! (and the size of the string)
+    fringe = deque()
+    visited = set()
+    fringe.append((start, 0))
+    visited.add(start)
+    parents = {}
+    while len(fringe) > 0:
+        v, _l = fringe.popleft()  # DIS IS PARENT
+        parents[_l] = v
+        if v == goal:
+            return v, _l, parents
+        for child in get_children(v, size):
+            # print(matstr(to_mat(child, size)))
+            if child not in visited:
+                fringe.append((child, _l + 1))  # ADD ONE TO PARENT"S LEVEL
+                visited.add(child)
+    return -1
 
 
-# def generate_puzzles(size):
-#     sizey = size * size
-#     s = ""
-#     for i in range(sizey - 1):
-#         s += chr(i + 65)
-#     s += "."
-#     perms = [''.join(p) for p in permutations(list(s))]
-#     return perms
+def generate_puzzles(size):
+    sizey = size * size
+    s = ""
+    for i in range(sizey - 1):
+        s += chr(i + 65)
+    s += "."
+    perms = ["".join(p) for p in permutations(list(s))]
+    return perms
+
 
 # ANSWERS TO QUESTIONS:
 """
@@ -231,21 +273,24 @@ if __name__ == "__main__":
     args = sys.argv
     path = args[1]
 
-    """
-    
     print(modBFS(2) + modBFS(3))
 
-    puzzles = generate_puzzles(3) 
+    puzzles = generate_puzzles(3)
 
     for puzzle in puzzles:
         goal = find_goal(puzzle)
         print(bfs(puzzle, goal, 3), puzzle, goal)
-        break # delete this to find all
+        break  # delete this to find all
 
     print(modBFS2("12345678.")[0])
 
     print(modBFS2("12345678."))
-    
+
+    # for i in modBFS3("8672543.1", "12345678.", 3)[-1].values():
+    #     print(i)
+
+    for i in modBFS3("64785.321", "12345678.", 3)[-1].values():
+        print(i)
 
     """
 
@@ -267,5 +312,4 @@ if __name__ == "__main__":
         c = time.perf_counter()
         print(f"Line {i}: {to_str(start)}, {level} moves found in {c-b} seconds")
     
-        
-
+    """
