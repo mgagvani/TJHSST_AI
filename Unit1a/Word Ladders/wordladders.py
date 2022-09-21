2e9e81521c415d025f50cf4804ba36cccf69226a
Manav Gagvani <mgagvani@outlook.com>
2022-09-21 14:14:16 -0400

    generate graph works (highly efficient) using binning

--- Unit1a/Word Ladders/wordladders.py
+++ Unit1a/Word Ladders/wordladders.py
@@ -27,13 +27,50 @@ def generate_graph(dictionary):
         child_lens.append(len(children))
     return allchild, child_lens
 
+def generate_graph2(dictionary):
+    data = {}
+    for word in dictionary:
+        for i in range(6):
+            newl = list(word); newl[i] = "?"
+            news = "".join(newl)
+            if news not in data:
+                data[news] = [word,]
+            else: 
+                data[news].append(word)
+
+    # for k in list(data):
+    #     if len(data[k]) == 1:
+    #         data.pop(k)
+
+    # i = 0
+    # for key in data.keys():
+    #     print(key, data[key])
+    #     i += 1
+    #     if i > 50:
+    #         break
+    graph = {}
+    for bin in data.keys():
+        for word in (x:=data[bin]):
+            if word not in graph:
+                graph[word] = set() # create a new set
+            for word2 in x:
+                if word2 != word:
+                    graph[word].add(word2)
+
+    # print(len(graph.keys()))
+    # i = 0
+    # for key in graph.keys():
+    #     print(key, graph[key])
+    #     i +=1
+    #     if i > 20:
+    #         break
+    return graph
 
 if __name__ == "__main__":
     time1 = perf_counter()
     dictionary = get_dictionary("words_06_letters.txt")
-    graph, lens = generate_graph(dictionary)
-    print(lens)
-    print(graph["beagle"])
+    graph = generate_graph2(dictionary)
+    print(graph["notion"])
     time2 = perf_counter()
     print(f"Time to create dictionary was: {time2-time1} seconds")
     print(f"There are {len(dictionary)} words in this dictionary. ")
