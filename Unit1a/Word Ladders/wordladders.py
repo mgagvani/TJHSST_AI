a417318205d3d5bcece974efe95a3fc7b44f62c6
Manav Gagvani <mgagvani@outlook.com>
2022-09-20 22:16:33 -0400

    generate graph (o n^2) bad times

--- Unit1a/Word Ladders/wordladders.py
+++ Unit1a/Word Ladders/wordladders.py
@@ -7,12 +7,33 @@ def get_dictionary(filePath):
         lines = set([line.strip() for line in file])
     return lines
 
+    [word1[i]==word2[i] for i in range(6)].count(False)==1
 
+def generate_graph(dictionary):
+    """
+    We want to pre-generate the entire graph once. 
+    """
+    allchild = {}
+    child_lens = []
+    for word in dictionary:
+        print(".",end="")
+        sys.stdout.flush()
+        children = set()
+        for word2 in dictionary:
+            # if [ord(a) ^ ord(b) for a, b in zip(word, word2)].count(0) == 5:
+            if [word[i]==word2[i] for i in range(6)].count(False) == 1:
+                children.add(word2)
+        allchild[word] = children
+        child_lens.append(len(children))
+    return allchild, child_lens
 
 
 if __name__ == "__main__":
     time1 = perf_counter()
     dictionary = get_dictionary("words_06_letters.txt")
+    graph, lens = generate_graph(dictionary)
+    print(lens)
+    print(graph["beagle"])
     time2 = perf_counter()
     print(f"Time to create dictionary was: {time2-time1} seconds")
     print(f"There are {len(dictionary)} words in this dictionary. ")
@@ -24,4 +45,5 @@ if __name__ == "__main__":
         line_list = [line.strip() for line in fileReader]
 
     for i, line in enumerate(line_list):
-        pass
+        start, goal = line.split(" ")
+        print(f"Start is {start} and goal is {goal}")
