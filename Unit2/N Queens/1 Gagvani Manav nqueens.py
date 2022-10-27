ecd53c7a95550441e7a5a9362f03e7a31ef1632e
Manav Gagvani <mgagvani@outlook.com>
2022-10-27 09:15:43 -0400

    finished nqueens

--- Unit2/N Queens/1 Gagvani Manav nqueens.py
+++ Unit2/N Queens/1 Gagvani Manav nqueens.py
@@ -2,8 +2,14 @@
 # Manav Gagvani - 10/18/22
 
 from time import perf_counter
+import random
 
-# Global SAFEIDX
+# use globals to prevent having to pass these around all over the place
+size = 0
+board = []
+row_conflicts =  []     
+diagr_conflicts = []   
+diagl_conflicts = []   
 
 def goal_test(state):
     return (not (None in state))
@@ -95,6 +101,134 @@ def nqueens_backtracking(state):
             return result
     return None
 
+def generate_initial_state(size):
+    # TODO this can be improved to come up with a decently good starting state
+    state = [a for a in range(size)]
+    random.shuffle(state)
+    return state
+
+def generate_state(size):
+    global board
+    # put one down every second column
+    # 0, 2, 4, etc. 
+    l = [None for _a in range(size)]
+    used = []
+    for i, j in enumerate(range(0, size, 2)):
+      l[i] = j
+      used.append(j)
+    allvals = [n for n in range(size)]
+    notused = list(set(allvals) - set(used))
+    for ii, vall in enumerate(l):
+      if vall is None:
+        l[ii] = notused.pop(0)
+    # return l
+    board = i
+
+def swap(state, x, y):
+    temp = state[x]
+    state[x] = state[y]
+    state[y] = temp # dont return cuz the pointer is still there? NOTE TODO 
+
+def random_swap(state):
+    a, b = random.randint(0, len(state)), random.randint(0, len(state))
+    swap(state, a, b)
+
+def change_conflicts(col, row, val):
+    row_conflicts[row] += val
+    diagr_conflicts[col + row] += val
+    diagl_conflicts[col + (size - row - 1)] += val
+
+def min_conflict_pos(col):
+    minConflicts = size
+    minConflictRows = []
+    for row in range(size):
+        conflicts = row_conflicts[row] + diagr_conflicts[col + row] + diagl_conflicts[col + (size - row - 1)]
+        if conflicts == 0:
+            return row
+        if conflicts < minConflicts:
+            minConflictRows = [row]
+            minConflicts = conflicts
+        elif conflicts == minConflicts:
+            minConflictRows.append(row)
+    # randomly choose the index from the list of tied conflict values
+    choice = random.choice(minConflictRows)
+    return choice
+
+# setup board so that it minimizes the conflicts initially
+def create_minimized_board():
+    global board
+    global row_conflicts
+    global diagr_conflicts
+    global diagl_conflicts
+    global size
+
+    board = []
+
+    # Initialize conflict arrs
+    diagr_conflicts = [0] * ((2 * size) - 1)
+    diagl_conflicts = [0] * ((2 * size) - 1)
+    row_conflicts = [0] * size
+
+    rowSet = set(range(0,size))
+    notPlaced = []
+
+    for col in range(0, size):
+        testRow = rowSet.pop()
+        conflicts = row_conflicts[testRow] + diagr_conflicts[col + testRow] + diagl_conflicts[col + (size- testRow - 1)]
+        if conflicts == 0:
+            board.append(testRow)
+            change_conflicts(col, board[col], 1)
+        else:
+            rowSet.add(testRow)
+            testRow2 = rowSet.pop()
+            conflicts2 = row_conflicts[testRow2] + diagr_conflicts[col + testRow2] + diagl_conflicts[col + (size- testRow2 - 1)]
+            if not conflicts2:
+                board.append(testRow2)
+                change_conflicts(col, board[col], 1)
+            else:
+                rowSet.add(testRow2)
+                board.append(None)
+                notPlaced.append(col)
+
+    for col in notPlaced:
+        board[col] = rowSet.pop()
+        change_conflicts(col, board[col], 1)
+
+    totalConflicts = sum(diagl_conflicts) + sum(diagr_conflicts) + sum(row_conflicts)
+
+    print(f"Conflicts: {totalConflicts} conflicts")
+
+
+def max_conflict_col():
+    conflicts, maxConflicts, maxConflictCols = 0, 0, []
+
+    for col in range(size):
+            row = board[col]
+            conflicts = row_conflicts[row] + diagr_conflicts[col+row] + diagl_conflicts[col+(size-row-1)]
+            if (conflicts > maxConflicts):
+                    maxConflictCols = [col]
+                    maxConflicts = conflicts
+            elif conflicts == maxConflicts:
+                    maxConflictCols.append(col)
+    choice = random.choice(maxConflictCols)
+    print(f"curr max conflicts: {maxConflicts}")
+    return choice, maxConflicts
+
+
+def nqueens_iterative3():
+    create_minimized_board()
+    iteration, maxIteration = 0, size
+
+    while (iteration < maxIteration): # so that it doesnt go forever
+        col, numConflicts = max_conflict_col()
+        if (numConflicts > 3): # cannot swap if there are only 2 bc infinte loop
+            newLocation = min_conflict_pos(col)
+            if (newLocation != board[col]):
+                change_conflicts(col, board[col], -1)
+                board[col] = newLocation
+                change_conflicts(col, newLocation, 1)
+        iteration += 1
+    return board
 
 def test_solution(state):
     for var in range(len(state)):
@@ -117,17 +251,21 @@ def test_solution(state):
 
 if __name__ == "__main__":
     start = perf_counter()
-    # sizes = (8,17,18,29,30,31,32)
-    sizes = [a for a in range(15, 30, 4)]
-    sizes.append(31)
-    sizes.append(35)
+    sizes = (19, 23, 29, 43, 49, 97)
+    # sizes = [a for a in range(15, 30, 4)]
+    # sizes.append(31)
+    # sizes.append(51)
     for i in sizes:
         state = [None for _b in range(i)]
 
         safeidx = [[a for a in range(i)] for b in range(i)]
 
         a =  perf_counter()
-        solution = nqueens_backtracking(state)
+        # METHOD GOES HERE
+        # solution = nqueens_backtracking(state)
+        # solution = nqueens_iterative(i, i**5)
+        size = i
+        solution = nqueens_iterative3()
         b = perf_counter()
 
         # aa = perf_counter()
